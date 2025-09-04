e818b7851394895ceca7af25f8012bc990266ef8
hadirida46 <hadirida46@gmail.com>
2025-09-04 14:37:39 +0300

    building migration

---
+++ database/migrations/2025_09_04_093813_create_buildings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('house_hub_id')->constrained('house_hubs')->cascadeOnDelete();
+            $table->string('name');
+            $table->integer('floor');
+            $table->integer('apartment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buildings');
+    }
+};
