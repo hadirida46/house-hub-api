4f7a7a9e6f3e891731289e3513a806c09063397b
hadirida46 <hadirida46@gmail.com>
2025-09-29 02:00:18 +0300

    integrate Vue into Laravel project

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hello from Vue Component 🎉</h1>
+</template>
+
+<script>
+export default {
+  name: "ExampleComponent"
+}
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +10,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
