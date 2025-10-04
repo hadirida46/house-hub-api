161d40ea821f56e7a4d8551bbb2ac17c23a1b3ba
hadirida46 <hadirida46@gmail.com>
2025-10-04 17:56:05 +0300

    React Integration

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HouseHub</title>
+{{--    @viteReactRefresh--}}
+{{--    @vite(['resources/js/'])--}}
+</head>
+<body>
+<div id="root"></div>
+</body>
+</html>
